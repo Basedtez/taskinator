2c33344f0cffc539cef79277eda23495e8eff7bf
Montez Burt <Griffenc4@gmail.com>
2022-05-26 05:56:46 -0500

    added edit function to task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -25,16 +27,62 @@ var createTaskEl = function (taskDataObj) {
   // create list item
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
+  listItemEl.setAttribute("data-task-id", taskIdCounter);
 
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
   listItemEl.appendChild(taskInfoEl);
+  var taskActionsE1 =createTaskActions(taskIdCounter);
+  listItemEl.appendChild(taskActionsE1);
+
+  tasksToDoEl.appendChild(listItemEl);
+  
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
+
+  taskIdCounter++;
 };
+
+var createTaskActions = function(taskId) {
+  var actionContainerE1 = document.createElement("div")
+  actionContainerE1.className = "task-actions";
+  var editButtonE1 = document.createElement("button");
+  editButtonE1.textContent = "Edit";
+  editButtonE1.className = "btn edit-btn";
+  editButtonE1.setAttribute("data-task-id", taskId);
+
+  actionContainerE1.appendChild(editButtonE1);
+
+  // create deletion button 
+  var deleteButtonE1 = document.createElement("button");
+  deleteButtonE1.textContent = "Delete";
+  deleteButtonE1.className = "btn delete-btn";
+  deleteButtonE1.setAttribute("data-task-id", taskId);
+
+  actionContainerE1.appendChild(deleteButtonE1);
+
+  var statusSelectE1= document.createElement("Select");
+  statusSelectE1.className = "select-status";
+  statusSelectE1.setAttribute("name", "status-change");
+  statusSelectE1.setAttribute("data-task-id", taskId);
+
+  actionContainerE1.appendChild(statusSelectE1);
+
+  var statusChoices = ["To Do", "In Progress", "Completed"];
+  for (var i = 0; i < statusChoices.length; i++) {
+    //create option element
+    var statusOptionE1 = document.createElement("option");
+    statusOptionE1.textContent = statusChoices[i];
+    statusOptionE1.setAttribute("value", statusChoices[i]);
+
+    statusSelectE1.appendChild(statusOptionE1);
+  }
+ 
+  return actionContainerE1;
+}
   
   
 
